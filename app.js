f292b0db2fb93b89e3e35c38292d0560f84c6974
Tim Ververs <timververs@Tims-MacBook-Pro.local>
2016-02-18 16:02:35 +0100

    Just give static HTML

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var express = require('express'),
     app = express();
 
 app.set('views', 'views');
+app.set("view options", {layout: false});
 
 app.get('/', function(req, res) {
-    res.render('home', {
+    res.sendfile('home.html');
   });
-});
 
 app.listen(8080);
 module.exports.getApp = app;
