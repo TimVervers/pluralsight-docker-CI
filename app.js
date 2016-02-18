4fb81319da7fe89210d471a27ecee5a537ed3275
Tim Ververs <timververs@Tims-MacBook-Pro.local>
2016-02-18 16:16:02 +0100

    Oke, maybe not

--- app.js
+++ app.js
@@ -5,8 +5,12 @@
 var express = require('express'),
     app = express();
 
-app.get('/',function(req,res){
-     res.sendFile('/views/home.html');
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+  });
 });
 
 app.listen(8080);
